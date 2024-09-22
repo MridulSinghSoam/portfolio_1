7da65ed5694f61c5e1d5aa25fcc5918d474c57a2
Mridul-ss <139462410+Mridul-ss@users.noreply.github.com>
2024-09-22 15:38:28 +0530

    email work

--- mail.js
+++
@@ -1,7 +0,0 @@
-function sendMail(){
-    let parms={
-        email:document.getElementById("email").value,
-        message:document.getElementById("message").value,
-    }
-    emailjs.send("service_rerp35n","template_f4k5bzu",parms).then(alert("yup ! i recived"));
-}
